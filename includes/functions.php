fd9712d15766ca4565d9db2f320dfade64f93e45
Massamba Sow <mass.sow@gmail.com>
2016-11-03 11:50:12 +0000

    refactored managed_contenet.php by moving sql queries to functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -6,4 +6,29 @@
 		}
 	}
 	
+	function find_all_subjects() {
+		global $connection;
+		
+		$query  = "SELECT * ";
+		$query .= "FROM subjects ";
+		$query .= "WHERE visible = 1 ";
+		$query .= "ORDER BY position ASC";
+		$subject_set = mysqli_query($connection, $query);
+		confirm_query($subject_set);
+		return $subject_set;
+	}
+	
+	function find_pages_for_subject($subject_id) {
+		global $connection;
+		
+		$query  = "SELECT * ";
+		$query .= "FROM pages ";
+		$query .= "WHERE visible = 1 ";
+		$query .= "AND subject_id = {$subject_id} ";
+		$query .= "ORDER BY position ASC";
+		$page_set = mysqli_query($connection, $query);
+		confirm_query($page_set); // $page_set is here a local variable
+		return $page_set;
+	}
+	
 ?>
