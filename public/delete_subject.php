cd07f0c6bff5f2df403c12604060750961aa6b63
Massamba Sow <mass.sow@gmail.com>
2016-11-11 13:21:33 +0000

    Housekeeping

--- public/delete_subject.php
+++ public/delete_subject.php
@@ -3,7 +3,6 @@
 <?php require_once("../includes/functions.php"); ?>
 
 <?php
-	/* Check whether the ID to be deleted is present in the DB */
 	$current_subject = find_subject_by_id($_GET["subject"]);
 	if (!$current_subject) {
 		// subject ID was missing or invalid or 
@@ -11,15 +10,13 @@
 		redirect_to("manage_content.php");
 	}
 	
-	/* Making sure we do not delete a subject and create orphaned pages.*/
 	$pages_set = find_pages_for_subject($current_subject["id"]);
 	if (mysqli_num_rows($pages_set) > 0) {
 		$_SESSION["message"] = "Can't delete a subject with pages.";
 		redirect_to("manage_content.php?subject={$current_subject["id"]}");
 	}
 	
-	/* finding and deleting */
-	$id = $current_subject["id"]; // not necessary step, just for reminder
+	$id = $current_subject["id"];
 	$query = "DELETE FROM subjects WHERE id = {$id} LIMIT 1";
 	$result = mysqli_query($connection, $query);
 
